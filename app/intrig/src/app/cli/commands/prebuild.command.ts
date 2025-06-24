4468f3589d2d073f930eec8e88c5812d36871613
tiran <tiran@intrigsoft.com>
2025-06-24 11:58:21 +0530

    chore: inject `GenerateCommand` into `PrebuildCommand` and invoke during execution

--- app/intrig/src/app/cli/commands/prebuild.command.ts
+++ app/intrig/src/app/cli/commands/prebuild.command.ts
@@ -7,6 +7,7 @@ import {PackageJson} from "nx/src/utils/package-json";
 import * as fsx from "fs-extra";
 import path from "path";
 import {ConfigService} from "@nestjs/config";
+import {GenerateCommand} from "./generate.command";
 
 @Command({name: "prebuild", description: "Prebuild."})
 export class PrebuildCommand extends CommandRunner {
@@ -15,11 +16,15 @@ export class PrebuildCommand extends CommandRunner {
 
   constructor(private pm: ProcessManagerService,
               @Inject(GENERATORS) private generators: GeneratorCli[],
-              private config: ConfigService) {
+              private config: ConfigService,
+              private generate: GenerateCommand,
+              ) {
     super();
   }
 
   override async run(passedParams: string[], options?: Record<string, any>): Promise<void> {
+    await this.generate.run([], {});
+
     const metadata = await this.pm.getMetadata();
     if (!metadata) {
       throw new Error("No metadata found");
